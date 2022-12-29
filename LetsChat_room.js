1a986b48cfd5f5f797564044efc6a0aed085b30f
farwa-faraz <99012312+farwa-faraz@users.noreply.github.com>
2022-12-29 12:28:50 +1100

    Add files via upload

---
+++ LetsChat_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAubFlYgE6AGSqY93IiSknZQAwKWG2dmBY",
+    authDomain: "letschat-46350.firebaseapp.com",
+    projectId: "letschat-46350",
+    storageBucket: "letschat-46350.appspot.com",
+    messagingSenderId: "1012688100951",
+    appId: "1:1012688100951:web:81d58668fc5a7499e34d12"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
